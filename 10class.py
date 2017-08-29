9aeef52b2ad3f023eb0563d4359c5c9533f4861b
yokochan20 <satooshi20sl@gmail.comp>
2017-08-29 11:27:52 +0900

    Commit

--- 10class.py
+++ 10class.py
@@ -10,48 +10,63 @@ from keras import optimizers
 x = np.load("/Users/SatoshiYokoyama/D-HACKS/image.npy")
 y = np.load("/Users/SatoshiYokoyama/D-HACKS/labels.npy")
 
-#one_hot vector
-y = label_binarize(y,classes=list(range(10)))
-#train:test=6:1
-x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=1./7)
-#train:valid=11:1
-x_train, x_valid, y_train, y_valid = train_test_split(x_train,y_train,test_size=1./12)
+#ラベルをone_hot vectorにする
+def one_hot():
+    global y
+    y = label_binarize(y,classes=list(range(10)))
+
+#train_test_valid_datasetを作成
+def mk_dataset():
+    #train:test=6:1
+    x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=1./7)
+    #train:valid=11:1
+    x_train, x_valid, y_train, y_valid = train_test_split(x_train,y_train,test_size=1./12)
+
 #モデル構築
+def model1():
+    #１種類の入力，１種類の出力だと簡単
+    model = Sequential()
+    model.add(Dense(500,input_shape=(784,)))
+    model.add(Activation("relu"))
+    model.add(Dense(10))
+    model.add(Activation("softmax"))
 
-#１種類の入力，１種類の出力だと簡単
-model = Sequential()
-model.add(Dense(500,input_shape=(784,)))
-model.add(Activation("relu"))
-model.add(Dense(10))
-model.add(Activation("softmax"))
-'''
-model = Sequential([
-    Dense(500,input_shape=(784,)),
-    Activation("relu"),
-    Dense(10),
-    Activation("softmax")
-    ])
+def model2():
+    model = Sequential([
+        Dense(500,input_shape=(784,)),
+        Activation("relu"),
+        Dense(10),
+        Activation("softmax")
+        ])
 
-model = Sequential()
-model.add(Dense(500,input_shape=(784,),activation="relu"))
-model.add(Dense(10,activation="softmax"))
+def model3():
+    model = Sequential()
+    model.add(Dense(500,input_shape=(784,),activation="relu"))
+    model.add(Dense(10,activation="softmax"))
 
-#複数の入力の時はこっち
-image = Input(shape=(784,))
-h = Dense(500)(image)
-h = Activation("relu")(h)
-h = Dense(10)(h)
-y = Activation("softmax")(h)
-model = Model(inputs=image,outputs=y)
-'''
+def model4():
+    #複数の入力の時はこっち
+    image = Input(shape=(784,))
+    h = Dense(500)(image)
+    h = Activation("relu")(h)
+    h = Dense(10)(h)
+    y = Activation("softmax")(h)
+    model = Model(inputs=image,outputs=y)
 
+def train1():
+    #学習
+    model.compile(optimizer="rmsprop",
+          loss="categorical_crossentropy",
+          metrics=["accuracy"])
 
-#学習
-model.compile(optimizer="rmsprop",
-      loss="categorical_crossentropy",
-      metrics=["accuracy"])
+    model.fit(x_train,y_train,batch_size=32,epochs=10)
 
-model.fit(x_train,y_train,batch_size=32,epochs=10)
+def evaluate1():
+    #評価
+    model.evaluate(x_test,y_test,metrics=["accuracy"])
 
-#評価
-model.evaluate(x_test,y_test,metrics=["accuracy"])
+one_hot()
+mk_dataset()
+model1()
+train1()
+evaluate1()
